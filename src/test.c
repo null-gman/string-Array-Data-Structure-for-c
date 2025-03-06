c8d60f39de9fb27e7a61141525f5fd7f704c62e6
null-gman <omar5mahmoud3ahmed6@gmail.com>
2025-03-06 15:33:39 +0200

    edit src/test.c : add test cases add comments

--- src/test.c
+++ src/test.c
@@ -1,31 +1,93 @@
+/*FILE test.c*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+/*
+typedef unsigned int UNINT ;
+
+typedef  struct strArr_t{
+    UNINT size;
+    UNINT len;
+    char ** elements;
+} strArr_t ;
++====================================+
+and the functions declaration
+*/
 #include "arrayOfStr.h"
 
 
 
-int main(int argc , char ** argv){
+
+int main(void){
+    /*testing the library*/
 
    strArr_t myData = createArrayOfStr();
+
+   puts("================");
+   puts("fun : push \"hello\",\"world\" :");
    pushStrArr(&myData,"hello");
    pushStrArr(&myData,"world");
-
-
    printStrArr(&myData);
+
    puts("================");
+   puts("fun : remove last string :");
    removeIndexStrArr(&myData, -1);
-
    printStrArr(&myData);
+
    puts("================");
+   puts("fun : push \"c\" :");
    pushStrArr(&myData,"c");
    printStrArr(&myData);
+
+   puts("================");
+   puts("fun : push \"programmers\" :");
+   pushStrArr(&myData,"programmers");
+   printStrArr(&myData);
+
+   puts("================");
+   puts("fun : replace \"programmers\" with \"developers\":");
+   replaceEleStrArr(&myData,-1,"developers");
+   printStrArr(&myData);
+
+   puts("================");
+   puts("fun : get  before last(arr.len - 2) element :");
+   char * myFavLang = getEleStrArr(&myData, -2);
+   puts(myFavLang);
+
+   puts("================");
+   puts("fun : printf the array with for loop:");
+   for (UNINT index = 0; index < myData.len; ++index) {
+       printf("[%d] \"%s\"",index,myData.elements[index]) ;
+       if (index < myData.len - 1) {
+           printf(" | ") ;
+       }
+   }
+   printf("\n") ;
+   puts("================");
+   puts("fun : printf the array after free it");
+   freeStrArr(&myData);
+   printStrArr(&myData); //will print nothing
+
+   puts("================");
+   puts("fun : acces element after free the array");
+   // puts(myData.elements); //Segmentation fault :)
+   // puts(myData.elements[0]); //Segmentation fault :)
+
    puts("================");
 
 
-   printf("%d\n",replaceEleStrArr(&myData,-1,"java"));
-   printStrArr(&myData);
-    puts("================");
-   freeStrArr(&myData);
+   /*
+    NOTE
+        > don't do that :
+            - arr.len = <your number>
+            - arr.size = <your number>
+            - arr.element = <your pointer>
+            - arr.element[index] = <your pointer>
+        > the functions will do that for you <3
+
+
+   */
+
    return  0;
 }
